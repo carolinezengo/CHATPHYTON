9befd0adba0f1bf32ed22085feb9ed4cb21bca5f
carolinezengo <carolinezengo@gmail.com>
2023-07-31 09:44:25 -0300

    Menssagem sendo enviada pelo tunel de msg

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 app = Flask(__name__)
-
+socketion = SocketIO(app, cors_allowed_origins="*")
+#funcao de enviar mensagem
+@socketion.on("message")
+def gerenciar_msg(mensagem):
+   send(mensagem, broadcast=True)
+   #criar a rota
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
-
+#rota do app
 if __name__ == '__main__':
-  app.run(debug=True) 
+ 
+ socketion.run(app, host="localhost")
